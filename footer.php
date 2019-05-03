069969466a7c892da1a9fff51c901000ff870a04
zakariamoutai <47528504+zakariamoutai@users.noreply.github.com>
2019-05-03 11:58:18 +0200

    Add files via upload
    header et footer

---
+++ footer.php
@@ -0,0 +1,2 @@
+<div class="footer-copyright text-center">&copy; 2019 Copyright | Droit
+d'auteur: webDynamique.ece.fr</div>
---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Projet WEB</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script
+src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="styles.css">
+<script type="text/javascript">
+$(document).ready(function(){
+$('.header').height($(window).height());
+});
+</script>
+</head>
+<body>
+<nav class="navbar navbar-expand-md">
+<a class="navbar-brand" href="#">Accueil</a>
+<button class="navbar-toggler navbar-dark" type="button" datatoggle="collapse" data-target="#main-navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="main-navigation">
+<ul class="navbar-nav">
+<li class="nav-item"><a class="nav-link"
+href="#">catégories</a></li>
+<li class="nav-item"><a class="nav-link" href="#">Ventes flash</a></li>
+<li class="nav-item"><a class="nav-link"
+href="#">Vendre</a></li>
+<li class="nav-item"><a class="nav-link"
+href="#">Votre Compte</a></li>
+<li class="nav-item"><a class="nav-link"
+href="#">Panier</a></li>
+<li class="nav-item"><a class="nav-link"
+href="#">Admin</a></li>
+</ul>
+</ul>
+</div>
+</nav>
+<header class="page-header header container-fluid">
+<div class="overlay"></div>
+<div class="description">
+</div>
+</header>
